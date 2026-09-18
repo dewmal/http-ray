3c5ee9619897df2d5f809ab945fdc4b880c3b372
dewmal <dewmal@ceylon.ai>
2026-09-18 23:29:03 +0530

    benchmmark freindly

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,8 @@ use std::{
     net::{SocketAddr, TcpListener, TcpStream},
 };
 
+use log::{debug, error, info};
+
 #[derive(Debug)]
 struct Request {
     pub method: String,
@@ -23,6 +25,8 @@ impl Request {
         let mut buffer = [0; 8];
         let mut request = Vec::new();
         let mut body = Vec::new();
+        let mut is_header_completed = false;
+        let mut content_length: usize = 0;
         loop {
             let n = stream.read(&mut buffer)?;
             if n == 0 {
@@ -32,25 +36,27 @@ impl Request {
 
             if let Some(header_left) = request.windows(4).position(|w| w == b"\r\n\r\n") {
                 let body_start = header_left + 4;
-                let header_content = String::from_utf8_lossy(&request[..header_left]);
-                let mut lines = header_content.lines();
+                if !is_header_completed {
+                    let header_content = String::from_utf8_lossy(&request[..header_left]);
+                    let mut lines = header_content.lines();
 
-                let request_line: Vec<_> = lines.next().expect("").split(" ").collect();
-                method = request_line[0].to_string();
-                path = request_line[1].to_string();
-                version = request_line[2].to_string();
-                for line in lines.skip(1) {
-                    let header_parts: Vec<_> = line.split(":").collect();
-                    let key = header_parts[0];
-                    let value = header_parts[1];
-                    header.insert(key.to_string(), value.trim().to_string());
+                    let request_line: Vec<_> = lines.next().expect("").split(" ").collect();
+                    method = request_line[0].to_string();
+                    path = request_line[1].to_string();
+                    version = request_line[2].to_string();
+                    for line in lines.skip(1) {
+                        let header_parts: Vec<_> = line.split(":").collect();
+                        let key = header_parts[0];
+                        let value = header_parts[1];
+                        header.insert(key.to_string(), value.trim().to_string());
+                    }
+                    is_header_completed = true;
+                    content_length = header
+                        .get("Content-Length")
+                        .unwrap_or(&"0".to_string())
+                        .parse::<usize>()
+                        .unwrap();
                 }
-
-                let content_length: usize = header
-                    .get("Content-Length")
-                    .expect("0")
-                    .parse::<usize>()
-                    .unwrap();
 
                 let body_recevied = request.len() - body_start;
                 if body_recevied >= content_length {
@@ -73,28 +79,26 @@ impl Request {
 fn handle_steam(mut stream: TcpStream) -> Result<(), io::Error> {
     let request = Request::from(&stream)?;
 
-    println!(
+    debug!(
         "Read From Request {} {} {} {:?}",
         request.method, request.path, request.version, request.body
     );
 
     let body = "Done";
     let head_text = "HTTP/1.1 200 OK";
-    let head_text = format!("{head_text}\r\nContent-Type: text/html");
+    let head_text = format!("{head_text}\r\nContent-Type: text/plain");
     let head_text = format!("{head_text}\r\nContent-Length: {}", body.len());
     let head_text = format!("{head_text}\r\nConnection: close\r\n\r\n");
 
     stream.write_all(head_text.as_str().as_bytes())?;
     stream.write_all(body.as_bytes())?;
     stream.flush()?;
-
-    stream.shutdown(std::net::Shutdown::Both)?;
-
     Ok(())
 }
 
 fn main() {
-    println!("Server started");
+    env_logger::init();
+    info!("Server started");
     let add_str = "127.0.0.1";
     let port = 23445;
     let socket_address: SocketAddr = format!("{add_str}:{port}").parse().unwrap();
@@ -102,17 +106,17 @@ fn main() {
     let port = socket_address.port();
     let ip = socket_address.ip();
 
-    println!("IP:{}, Port:{}", ip, port);
+    info!("IP:{}, Port:{}", ip, port);
 
     let listerner = TcpListener::bind(socket_address).unwrap();
     for stream_raw in listerner.incoming() {
         match stream_raw {
             Ok(stream) => match handle_steam(stream) {
-                Ok(_) => println!("Done"),
-                Err(er) => println!("Error :{:?}", er),
+                Ok(_) => debug!("Request Processed"),
+                Err(er) => error!("Error :{:?}", er),
             },
             Err(er) => {
-                println!("Error :{:?}", er);
+                error!("Error :{:?}", er);
             }
         }
     }
